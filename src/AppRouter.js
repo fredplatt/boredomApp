3b47146f2ca2ff3dd583609d2f3d714d3207468a
fredplatt <fred_platt@hotmail.com>
2019-05-24 13:43:25 +0100

    changed App to AppRouter and coded nav bar plus its routes

---
+++ src/AppRouter.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+function AppRouter() {
+  return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/education/">Educational</Link>
+              </li>
+              <li>
+                <Link to="/recreational/">Recreational</Link>
+              </li>
+              <li>
+                <Link to="/social/">Social</Link>
+              </li>
+              <li>
+                <Link to="/diy/">DIY</Link>
+              </li>
+              <li>
+                <Link to="/charity/">Charity</Link>
+              </li>
+              <li>
+                <Link to="/cooking/">Cooking</Link>
+              </li>
+              <li>
+                <Link to="/relaxation/">Relaxation</Link>
+              </li>
+              <li>
+                <Link to="/music/">Music</Link>
+              </li>
+              <li>
+                <Link to="/busywork/">Busywork</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact />
+          <Route path="/education/" component={Educational} />
+          <Route path="/recreational/" component={Recreational} />
+          <Route path="/social/" component={Social} />
+          <Route path="/diy/" component={Diy} />
+          <Route path="/charity/" component={Charity} />
+          <Route path="/cooking/" component={Cooking} />
+          <Route path="/relaxation/" component={Relaxation} />
+          <Route path="/music/" component={Music} />
+          <Route path="/busywork/" component={Busywork} />
+        </div>
+      </Router>
+  );
+}
+
+export default AppRouter;
+
+
+
+//education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"
