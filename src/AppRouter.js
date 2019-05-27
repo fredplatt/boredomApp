f5524d950878709df49c8c375110b176d7cf4044
fredplatt <fred_platt@hotmail.com>
2019-05-27 11:21:03 +0100

    added favicon, changed title

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 import Home from './components/Home'
